57845654623d7a05ac14248333d503d3d9bcb75c
ahsanshaikh1314 <m.ahsan.shaikh2203@gmail.com>
2026-05-19 10:42:01 +0500

    FIX port on Selenium Frontend Test

--- selenium/test_taskmanager.py
+++ selenium/test_taskmanager.py
@@ -20,7 +20,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
-APP_URL = os.environ.get("APP_URL", "http://localhost:80")
+APP_URL = os.environ.get("APP_URL", "http://localhost:8081")
 
 
 def get_driver():
